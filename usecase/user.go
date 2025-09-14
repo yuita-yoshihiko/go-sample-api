86347fc37eb9b83c6ed6bc978a2078a46ddb0fb5
yuita-yoshihiko <soup998211@gmail.com>
2025-09-14 17:48:13 +0900

    user usecase

--- usecase/user.go
+++ usecase/user.go
@@ -2,37 +2,46 @@ package usecase
 
 import (
 	"context"
+
 	"github.com/yuita-yoshihiko/go-sample-api/infrastructure/db"
-	"github.com/yuita-yoshihiko/go-sample-api/usecase/converter"
+	"github.com/yuita-yoshihiko/go-sample-api/models"
 	"github.com/yuita-yoshihiko/go-sample-api/usecase/repository"
 )
 
 type UserUseCase interface {
-	Fetch(context.Context, int64) (*converter.UserOutput, error)
+	Fetch(context.Context, int64) (*models.User, error)
+	Create(context.Context, *models.User) error
+	Update(context.Context, *models.User) error
+	Delete(context.Context, int64) error
 }
 
 type userUseCaseImpl struct {
 	dbUtils    db.DBUtils
 	repository repository.UserRepository
-	converter  converter.UserConverter
 }
 
 func NewUserUseCase(
 	du db.DBUtils,
 	r repository.UserRepository,
-	c converter.UserConverter,
 ) UserUseCase {
 	return &userUseCaseImpl{
 		dbUtils:    du,
 		repository: r,
-		converter:  c,
 	}
 }
 
-func (u *userUseCaseImpl) Fetch(ctx context.Context, id int64) (*converter.UserOutput, error) {
-	m, err := u.repository.Fetch(ctx, id)
-	if err != nil {
-		return nil, err
-	}
-	return u.converter.ToUserOutput(m), nil
+func (u *userUseCaseImpl) Fetch(ctx context.Context, id int64) (*models.User, error) {
+	return u.repository.Fetch(ctx, id)
+}
+
+func (u *userUseCaseImpl) Create(ctx context.Context, user *models.User) error {
+	return u.repository.Create(ctx, user)
+}
+
+func (u *userUseCaseImpl) Update(ctx context.Context, user *models.User) error {
+	return u.repository.Update(ctx, user)
+}
+
+func (u *userUseCaseImpl) Delete(ctx context.Context, id int64) error {
+	return u.repository.Delete(ctx, id)
 }
